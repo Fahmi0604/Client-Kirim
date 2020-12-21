84eb853e068ab9c96f59fa75da4e5dc9b6e51a0f
fahmi0604 <fahmifirmansyah917@gmail.com>
2020-12-21 22:49:01 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const upload = require('express-fileupload')
+
+const app = express()
+
+app.use(upload())
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
+    if (req.files){
+        console.log(req.files)
+        var file = req.files.file
+        var filename = file.name
+    }
+})
+
+app.listen(5000)
