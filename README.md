543561234554974f6c73e937a658d2a395178177
Unero <ramadhanbhg@gmail.com>
2020-12-25 18:48:05 +0700

    feat: IP Block

---
+++ README.md
@@ -0,0 +1,16 @@
+# Client-Kirim
+## Project UAS Mata Kuliah Pemrograman Jaringan
+## WebSocket kirim file text lalu read
+  
+### Kelompok
+- Fahmi  
+- Unero
+  
+### Teknologi
+- Express JS (Backend)
+
+
+### Library 🔧
+- Express ip-filter
+- Express fileUpload
+- CORS
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const upload = require('express-fileupload')
+const ipfilter = require('express-ipfilter').IpFilter
+
 const myFolder = './uploads/';
 var fs = require('fs')
 
 const app = express()
 
 app.use(upload())
+app.listen(5000, function () {
+    console.log("Listen at 5000")
+})
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    var trustedIps = ['::1']; // Only Server can Access
+    var requestIP = req.connection.remoteAddress;
+    if(trustedIps.indexOf(requestIP) >= 0) {
+        res.sendFile(__dirname + '/index.html')
+    } else {
+        res.send("Sorry, No access")
+    }
 })
 
 app.post('/', (req, res) => {
@@ -34,6 +45,4 @@ app.get('/folder', (req, res) => {
     });
 })
 
-app.listen(5000, function () {
-    console.log("Listen at 5000")
-})
+module.exports = app
